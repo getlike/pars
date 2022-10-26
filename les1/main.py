1af3c795c703240c0e835ec777b289e71241dfc5
getlike <itsr.org@gmail.com>
2022-10-26 23:38:20 +0200

    живое)))

--- les1/main.py
+++ les1/main.py
@@ -5,9 +5,9 @@ headers = {
     "accept": "*/*",
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
 }
-url = "https://arduino.ua/cat340-xiaomi"
+url = "https://transsibinfo.com/news"
 soup = requests.get(url, headers=headers)
 
 src = soup.text
-
-print(src)
+with open('index.html', 'w') as file:
+    file.write(src)
